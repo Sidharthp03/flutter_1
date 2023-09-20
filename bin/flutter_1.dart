e0b6b8b51fa912dcd82f67ee612b57eac61e5a56
Sidharthp03 <sidharthharidas03@gmail.com>
2023-09-21 00:50:26 +0530

    commit

--- bin/flutter_1.dart
+++ bin/flutter_1.dart
@@ -1,24 +1,89 @@
-class cars{
-  String?name;
-  int?price;
-  String?brand;
-  double?mileage;
-  dynamic colour;
-}
-void main() {
-  cars ct1=cars();
-  print("the name of cars is ${ct1.name="SELTOS"}");
-  print("The price is ${ct1.price=1000000}");
-  print("the brand name is ${ct1.brand="KIA"}");
-  print("the milage is ${ct1.mileage=25.6}");
-  print("the colour is ${ct1.colour="red"}");
+// DAY 4 SEP 19th
 
-  print("-------------------------------------");
+// class cars{
+//   String?name;
+//   int?price;
+//   String?brand;
+//   double?mileage;
+//   dynamic colour;
+// }
+// void main() {
+//   cars ct1=cars();
+//   print("the name of cars is ${ct1.name="SELTOS"}");
+//   print("The price is ${ct1.price=1000000}");
+//   print("the brand name is ${ct1.brand="KIA"}");
+//   print("the milage is ${ct1.mileage=25.6}");
+//   print("the colour is ${ct1.colour="red"}");
 
-  cars ct2=cars();
-  print("the name of the car is ${ct2.name="alto"}");
-  print("the price of the car is ${ct2.price=200000}");
-  print("the brand name is ${ct2.brand="MARUTHI"}");
-  print("the milage of the car is ${ct2.mileage=20.65}");
-  print("the colur of the car is ${ct2.colour="white"}");
-}
+//   print("-------------------------------------");
+
+//   cars ct2=cars();
+//   print("the name of the car is ${ct2.name="alto"}");
+//   print("the price of the car is ${ct2.price=200000}");
+//   print("the brand name is ${ct2.brand="MARUTHI"}");
+//   print("the milage of the car is ${ct2.mileage=20.65}");
+//   print("the colour of the car is ${ct2.colour="white"}");
+// }
+
+// DAY 5 SEP 20th
+
+// //ADDITION
+
+// void addition(){
+// int a = 100, b = 200; ///here a and b are locall variables
+// print("sum = ${a+b}");
+// }
+// void main(){
+//   addition();
+// }
+
+// // //SUBSTRACTION
+
+// // void substraction(){
+// //   int a = 10, b = 20;
+// //   print("sum = ${a-b}");
+// // }
+// void main(){
+//   substraction();
+// }
+
+//MULTIPLICATION
+
+// void MULTIPLICATION(){
+//   int a = 50, b = 25;
+//   print("sum = ${a*b}");
+// }
+// void main(){
+//   MULTIPLICATION();
+// }
+
+// // class flutter{
+// //   int c = 200;
+
+// //   void addition(){
+// //     int a = 100, b = 200;
+// //     print("sum=${a+b+c}");
+// //   }
+// //   }
+// //   void main(){
+// //     flutter obj = flutter();
+// //     obj.addition();
+
+// }
+
+import 'dart:io';
+
+// void main() {
+//   print("Enter your name : ");
+//   String name = stdin.readLineSync()!;
+//   print("enter your age : ");
+//   int age = int.parse(stdin.readLineSync()!);
+//   print("enter your cgpa : ");
+//   double cgpa = double.parse(stdin.readLineSync()!);
+
+//   print("name : $name");
+//   print("age : $age");
+//   print("cgpa : $cgpa");
+// }
+
+
